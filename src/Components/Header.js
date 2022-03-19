b6d7ee83942d88207b55c534cc6c6e057b57a7ec
ptoonzpheulz <oyinloladawudoluwapelumi@gmail.com>
2022-03-19 03:24:31 +0100

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import bg from '../Images/bg.svg';
+import HeaderCss from './Header.module.css';
+
+
+function Header() {
+  return (
+    <div className={HeaderCss.head}>
+      </div>
+  )
+}
+
+export default Header
